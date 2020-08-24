034c89efce8a7377c6af70f9fab5e68f92dcd813
Diana Arismendi <dianajara82@gmail.com>
2020-08-23 20:13:38 -0500

    Tarea visual

---
+++ Tarea1/Form1.Designer.cs
@@ -0,0 +1,111 @@
+﻿namespace Tarea1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnsaludar = new System.Windows.Forms.Button();
+            this.txtnombre = new System.Windows.Forms.TextBox();
+            this.picImagen = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.picImagen)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.label1.Font = new System.Drawing.Font("MS Reference Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.label1.Location = new System.Drawing.Point(379, 31);
+            this.label1.Margin = new System.Windows.Forms.Padding(8, 0, 8, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(345, 40);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Ingresa Tu Nombre";
+            // 
+            // btnsaludar
+            // 
+            this.btnsaludar.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.btnsaludar.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnsaludar.Location = new System.Drawing.Point(474, 139);
+            this.btnsaludar.Margin = new System.Windows.Forms.Padding(8, 7, 8, 7);
+            this.btnsaludar.Name = "btnsaludar";
+            this.btnsaludar.Size = new System.Drawing.Size(97, 42);
+            this.btnsaludar.TabIndex = 1;
+            this.btnsaludar.Text = "Saludar!";
+            this.btnsaludar.UseVisualStyleBackColor = false;
+            this.btnsaludar.Click += new System.EventHandler(this.btnsaludar_Click);
+            // 
+            // txtnombre
+            // 
+            this.txtnombre.Font = new System.Drawing.Font("Microsoft YaHei UI", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtnombre.Location = new System.Drawing.Point(425, 87);
+            this.txtnombre.Name = "txtnombre";
+            this.txtnombre.Size = new System.Drawing.Size(195, 42);
+            this.txtnombre.TabIndex = 2;
+            // 
+            // picImagen
+            // 
+            this.picImagen.Image = ((System.Drawing.Image)(resources.GetObject("picImagen.Image")));
+            this.picImagen.Location = new System.Drawing.Point(23, 12);
+            this.picImagen.Name = "picImagen";
+            this.picImagen.Size = new System.Drawing.Size(336, 178);
+            this.picImagen.TabIndex = 3;
+            this.picImagen.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(17F, 31F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.ClientSize = new System.Drawing.Size(771, 328);
+            this.Controls.Add(this.picImagen);
+            this.Controls.Add(this.txtnombre);
+            this.Controls.Add(this.btnsaludar);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(8, 7, 8, 7);
+            this.Name = "Form1";
+            this.Text = "Saludador";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.picImagen)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnsaludar;
+        private System.Windows.Forms.TextBox txtnombre;
+        private System.Windows.Forms.PictureBox picImagen;
+    }
+}
+
---
+++ Tarea1/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Tarea1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnsaludar_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Si es tu Nombre?",
+                "Mensaje de Bienvenida",
+                MessageBoxButtons.OKCancel,
+                MessageBoxIcon.Warning);
+
+            MessageBox.Show("Hola!" + txtnombre.Text,
+                "Saludo a Usuario",
+                MessageBoxButtons.OK,
+                MessageBoxIcon.Information);
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
